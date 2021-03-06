a2c1c34d130a02386b9263bb11270f412287bc28
AryanSteve1118 <72647556+AryanSteve1118@users.noreply.github.com>
2021-03-06 14:08:59 +0530

    Add files via upload

---
+++ project97/Guessthenumber.py
@@ -0,0 +1,17 @@
+import random
+number=random.randint(1,9)
+chances=0
+while chances < 5 :
+    un=int(input("Enter the number : "))
+    if(number==un):
+        print("Congratulation You WON")
+        break
+    elif(un<number):
+        print("your guess is too low: guess a number higher than ",un)
+        break
+    else:
+        print("your guess is too high: guess a number lower than ",un)
+        break
+    chances+=1
+if(not chances < 5):
+    print("YOU LOSE !!! The Number is ",number)
